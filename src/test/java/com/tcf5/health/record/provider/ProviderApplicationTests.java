833c664d4266e8102ae13addf38b00d9c4b03174
Diego Zito <diegomirandazito@gmail.com>
2026-03-28 15:19:20 -0300

    subindo projeto base, sem configuracoes

---
+++ src/test/java/com/tcf5/health/record/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcf5.health.record.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
